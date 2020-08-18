58943ef2f20f9361fdfbe10ca732d94109604a63
biancamusat <biancamusat.98@gmail.com>
2020-08-19 00:09:11 +0300

    Bullets & more

---
+++ Assets/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m/s")] [SerializeField] float xSpeed = 4f;
+    [Tooltip("In m/s")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m/s")] [SerializeField] float yRange = 5f;
+
+    [SerializeField] float posPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float posYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow, yThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * posPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * posYawFactor;
+        float roll = xThrow * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        rawNewXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * xSpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        rawNewYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(rawNewXPos, rawNewYPos, transform.localPosition.z);
+    }
+}
