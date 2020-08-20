7e9da51e438a3422d83766026c4cf59e7c689674
biancamusat <biancamusat.98@gmail.com>
2020-08-20 18:51:13 +0300

    Exploding enamies and scoreboard

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 100;
+
+    ScoreBoard scoreBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        scoreBoard.AddScore(scorePerHit);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int _score;
+    DateTime _startTime;
+    public int Score { 
+        get {
+            return _score + (int) (DateTime.Now - _startTime).TotalSeconds;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _score = 0;
+        _startTime = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        var text = GetComponent<Text>();
+        text.text = Score.ToString();
+    }
+
+    public void AddScore(int scorePerHit)
+    {
+        _score += scorePerHit;
+    }
+}
